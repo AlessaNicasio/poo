63bd1f168f3b9beaaa2b0d4bc550a45012efb221
AlessaNicasio <60447974+AlessaNicasio@users.noreply.github.com>
2020-03-10 23:47:09 -0700

    Add files via upload

---
+++ actores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace actores
+{
+    class actores
+    {
+        private string nombre;
+        public actores(string n)
+        {
+            nombre = n;
+        }
+        public void imprime()
+        {
+            Console.WriteLine(nombre);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<actores> Actor = new List<actores>();
+            Actor.Add(new actores("Daniel Day-Lewis"));
+            Actor.Add(new actores("Jack Nicholson"));
+            Actor.Add(new actores("Gary Cooper"));
+            Actor.Add(new actores("Tom Hanks "));
+ 
+            foreach(actores a in Actor)
+            {
+                a.imprime();
+            }
+            
+        }
+    }
+}
